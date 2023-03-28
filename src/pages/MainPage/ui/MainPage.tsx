6e48bb05fb1f62b0b79b98bd7e5c9fdbf216e213
Tatyana Mirzova <44976901+sealione1311@users.noreply.github.com>
2023-03-29 00:43:16 +0300

    add error page and error boundary

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { BugButton } from 'app/providers/ErrorBoundary';
 
 function MainPage() {
   return (
     <div>
+      {/* eslint-disable-next-line i18next/no-literal-string */}
+      <BugButton />
       MAIN
     </div>
   );
